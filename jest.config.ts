354e596e250765a2b7f4e20c5295ac837ed292e7
Caducoder <carlosamericodev@gmail.com>
2024-04-20 12:15:31 -0300

    chore: install jest

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ */
+
+import type { Config } from "jest";
+
+const config: Config = {
+  clearMocks: true,
+  coverageProvider: "v8",
+};
+
+export default config;
